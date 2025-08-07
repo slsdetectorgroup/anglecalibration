99278bf84722fd35eeddd746ea4490228cce9aab
Alice <alice.mazzoleni@psi.ch>
2025-08-07 19:17:39 +0200

    added __init__.py file

---
+++ python/angcal/__init__.py
@@ -0,0 +1,11 @@
+from . import _angcal
+
+from ._angcal import AngleCalibration 
+
+from ._angcal import MythenFileReader
+
+from ._angcal import FlatField
+
+from ._angcal import MythenDetectorSpecifications
+
+from ._angcal import MythenFileReader 
