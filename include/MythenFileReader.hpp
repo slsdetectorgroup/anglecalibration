c3d4e210eefce5e82ae8e0f0a8b0ec5c06ce05f4
Alice <alice.mazzoleni@psi.ch>
2025-09-30 17:43:43 +0200

    changed GIT_TAG back to main

--- include/MythenFileReader.hpp
+++ include/MythenFileReader.hpp
@@ -10,22 +10,39 @@
 #include <string>
 
 #include "Hdf5FileReader.hpp"
-#include "aare/NDArray.hpp"
-#include "aare/RawMasterFile.hpp"
-#include "aare/File.hpp"
 #include "aare/Frame.hpp"
+#include "aare/NDArray.hpp"
+#include "aare/RawFile.hpp"
+#include "aare/RawMasterFile.hpp"
 
 using namespace aare;
 namespace angcal {
 
+/**
+ * @brief converts channel mask to bit mask e.g. 4 -> {1 0 0} only channel 0 is
+ * enabled
+ */
+inline std::array<uint8_t, 3>
+counter_mask_to_channel_indices(uint8_t counter_mask) {
+    std::bitset<3> binary_channel_numbers(counter_mask); // 1 0 0
+
+    // binary_channel_numbers.flip(); // TODO not sure where most
+    // significant
+    //  bit is ask Anna again
+
+    return std::array<uint8_t, 3>{binary_channel_numbers[0],
+                                  binary_channel_numbers[1],
+                                  binary_channel_numbers[2]};
+}
+
 struct MythenFrame : public aare::Frame {
-    //NDArray<uint32_t, 1> photon_counts{};
+    // NDArray<uint32_t, 1> photon_counts{};
     double detector_angle{};
-    double incident_intensity{}; // I_0
+    uint64_t incident_intensity{}; // I_0
     std::array<uint8_t, 3> channel_mask{};
 
     MythenFrame() = delete;
-    MythenFrame(aare::Frame &&frame) : aare::Frame(std::move(frame)) {};
+    MythenFrame(aare::Frame &&frame) : aare::Frame(std::move(frame)){};
     MythenFrame(MythenFrame &&) = default; // important for move
 
     NDView<uint32_t, 1> photon_counts() {
@@ -40,11 +57,25 @@ struct MythenFrame : public aare::Frame {
     }
 };
 
+/**
+ * @brief MythenFileReader to read raw files in the sls detector format
+ * @param detector_angles path to file storing detector angles of the
+ * acquisitions (need to be stored as binary file of doubles)
+ * @param incident_intensities path to file storing incident intensities of the
+ * acquisitions (need to be stored as binary file of uint64_t)
+ */
 class MythenFileReader {
 
   public:
     MythenFileReader() = default;
 
+    /**
+     * @brief Constructor for MythenFileReader class
+     * @param detector_angles path to file storing detector angles of the
+     * acquisitions (need to be stored as binary file of doubles)
+     * @param incident_intensities path to file storing incident intensities of
+     * the acquisitions (need to be stored as binary file of uint64_t)
+     */
     MythenFileReader(const std::filesystem::path &detector_angles,
                      const std::filesystem::path &incident_intensities)
         : detector_angles_filename(detector_angles),
@@ -54,13 +85,13 @@ class MythenFileReader {
         try {
             detector_angles_file.open(detector_angles_filename,
                                       std::ios_base::in);
-        } catch (const std::exception& error) {
+        } catch (const std::exception &error) {
             LOG(TLogLevel::logERROR) << "Could not open file\n";
         }
         try {
             incident_intensities_file.open(incident_intensities_filename,
                                            std::ios_base::in);
-        } catch (const std::exception& error) {
+        } catch (const std::exception &error) {
             LOG(TLogLevel::logERROR) << "Could not open file\n";
         }
     };
@@ -70,6 +101,9 @@ class MythenFileReader {
         incident_intensities_file.close();
     }
 
+    /**
+     * @brief reads the detector angle from file
+     */
     void read_detector_angle(const size_t acquisition_index,
                              double &detector_angle) {
         detector_angles_file.seekg(
@@ -80,22 +114,33 @@ class MythenFileReader {
     }
 
     void read_incident_intensity(const size_t acquisition_index,
-                                 uint64_t incident_intensity) {
-        detector_angles_file.seekg(
+                                 uint64_t &incident_intensity) {
+        // TODO: add a contract check that it is a binary file of uint64_t
+        incident_intensities_file.seekg(
             acquisition_index *
             sizeof(uint64_t)); // what if its not stored as a double
-        detector_angles_file.read(reinterpret_cast<char *>(&incident_intensity),
-                                  sizeof(uint64_t));
+        incident_intensities_file.read(
+            reinterpret_cast<char *>(&incident_intensity), sizeof(uint64_t));
     }
 
+    /**
+     * @brief read Mythenframe
+     * @warning the filename needs to include the acquisition index starting at
+     * zero
+     */
     MythenFrame read_frame(const std::filesystem::path &filename) {
 
-        aare::File file(filename); // expects raw file
-
-        // extract acquisition_index
+        aare::RawFile file(filename); // expects raw file
 
         MythenFrame frame = static_cast<MythenFrame>(file.read_frame());
 
+        auto counter_mask = file.master().counter_mask();
+
+        if (counter_mask.value())
+            frame.channel_mask =
+                counter_mask_to_channel_indices(counter_mask.value());
+
+        // extract acquisition_index
         size_t acquisition_index = get_acquisition_index(filename);
 
         read_incident_intensity(acquisition_index, frame.incident_intensity);
@@ -120,19 +165,15 @@ class MythenFileReader {
     std::string incident_intensities_filename{};
     // NDArray<double> detector_angles{};
     // NDArray<uint32_t> incident_intensities{}; // How many bytes do we need?
-    std::ifstream detector_angles_file;
-    std::ifstream incident_intensities_file;
+    std::ifstream detector_angles_file{};
+    std::ifstream incident_intensities_file{};
 };
 
-/** minimal version for a mythen file reader */
+/** minimal version for custom mythen file reader */
 class CustomMythenFileReader : public HDF5FileReader, public MythenFileReader {
 
   public:
-    CustomMythenFileReader() = default;
-
-    CustomMythenFileReader(
-        const std::filesystem::path &incident_intensity_filename)
-        : MythenFileReader(incident_intensity_filename, "") {};
+    CustomMythenFileReader() : MythenFileReader("", ""){};
 
     /**
      * @brief read acquisition file from hdf5
@@ -146,11 +187,6 @@ class CustomMythenFileReader : public HDF5FileReader, public MythenFileReader {
         auto dataset_photon_count =
             get_dataset("/entry/instrument/detector/data");
 
-        /*
-        frame.photon_counts =
-            dataset_photon_count.store_as_ndarray<uint32_t, 1>();
-        */
-
         MythenFrame frame =
             static_cast<MythenFrame>(dataset_photon_count.store_as_frame());
 
@@ -160,10 +196,14 @@ class CustomMythenFileReader : public HDF5FileReader, public MythenFileReader {
         dataset_detector_angle.read_into_buffer(
             reinterpret_cast<std::byte *>(&frame.detector_angle));
 
+        /*
         size_t acquisition_index = get_acquisition_index(file_name);
 
         MythenFileReader::read_incident_intensity(acquisition_index,
                                                   frame.incident_intensity);
+        */
+
+        read_incident_intensity(frame.incident_intensity);
 
         auto dataset_channel_number =
             get_dataset("/entry/instrument/NDAttributes/CounterMask");
@@ -173,15 +213,7 @@ class CustomMythenFileReader : public HDF5FileReader, public MythenFileReader {
         dataset_channel_number.read_into_buffer(
             reinterpret_cast<std::byte *>(&channel_number));
 
-        std::bitset<3> binary_channel_numbers(channel_number); // 1 0 0
-
-        // binary_channel_numbers.flip(); // TODO not sure where most
-        // significant
-        //  bit is ask Anna again
-
-        frame.channel_mask = std::array<uint8_t, 3>{binary_channel_numbers[0],
-                                                    binary_channel_numbers[1],
-                                                    binary_channel_numbers[2]};
+        frame.channel_mask = counter_mask_to_channel_indices(channel_number);
 
         close_file();
 
@@ -190,7 +222,7 @@ class CustomMythenFileReader : public HDF5FileReader, public MythenFileReader {
 
     void read_incident_intensity(uint64_t incident_intensity) {
         auto dataset_incident_intensity =
-            get_dataset("/entry/instrument/NDAttributes/I0");
+            get_dataset("/entry/instrument/NDAttributes/Izero");
 
         dataset_incident_intensity.read_into_buffer(
             reinterpret_cast<std::byte *>(&incident_intensity));
@@ -198,19 +230,21 @@ class CustomMythenFileReader : public HDF5FileReader, public MythenFileReader {
 };
 
 /**
-* @brief store as array
-* @param filename full path to file, file needs to have one value per line
-*/
+ * @brief store as array
+ * @param filename full path to file, file needs to have one value per line
+ */
 template <typename T>
 NDArray<T> store_as_array(const std::filesystem::path &filename) {
 
     std::ifstream file;
-    file.open(filename); //do i need to open with std::ios::ate?
-    
-    size_t lineCount = std::count(std::istreambuf_iterator<char>(file),
-                                  std::istreambuf_iterator<char>(), '\n') + 1; //TODO: buggy, think about other filesystem - raw, binary file easier
+    file.open(filename); // do i need to open with std::ios::ate?
 
-    NDArray<T, 1> array{std::array<ssize_t,1>{lineCount}};
+    size_t lineCount = std::count(std::istreambuf_iterator<char>(file),
+                                  std::istreambuf_iterator<char>(), '\n') +
+                       1; // TODO: buggy, think about other filesystem - raw,
+                          // binary file easier
+
+    NDArray<T, 1> array{std::array<ssize_t, 1>{lineCount}};
 
     size_t index = 0;
     std::string line{};
@@ -218,7 +252,7 @@ NDArray<T> store_as_array(const std::filesystem::path &filename) {
     try {
         while (std::getline(file, line)) {
             array(index) = static_cast<T>(line);
-            ++index; 
+            ++index;
         }
     } catch (const std::exception &e) {
         LOG(TLogLevel::logERROR) << e.what();
--- src/MythenFileReader.test.cpp
+++ src/MythenFileReader.test.cpp
@@ -16,13 +16,59 @@
 using namespace aare;
 using namespace angcal;
 
-TEST_CASE("test mythenfile_reader", "[.mythenfilereader][.files]") {
+TEST_CASE("MythenFileReader using raw file", "[.mythenfilereader][.files]") {
+
+    SECTION("read mythen frame from raw file") {
+
+        MythenFileReader file_reader;
+
+        auto mythen_data = test_data_path() / "AngleCalibration_Test_Data" /
+                           "DummyRawFiles" / "run_2_master_2.json";
+
+        REQUIRE(std::filesystem::exists(mythen_data));
+
+        MythenFrame frame = file_reader.read_frame(mythen_data);
+
+        REQUIRE(frame.detector_angle == 0);
+        REQUIRE(frame.incident_intensity == 0);
+        REQUIRE(frame.photon_counts().size() == 1280 * 2);
+        REQUIRE(frame.channel_mask == std::array<uint8_t, 3>{0, 0, 1});
+    }
+    SECTION(
+        "read mythen frame from raw file including detector and I0 values") {
+
+        auto mythen_data = test_data_path() / "AngleCalibration_Test_Data" /
+                           "DummyRawFiles" / "run_2_master_2.json";
+
+        REQUIRE(std::filesystem::exists(mythen_data));
+
+        auto I0_data = test_data_path() / "AngleCalibration_Test_Data" /
+                       "DummyRawFiles" / "I0.bin";
+
+        auto detector_angles = test_data_path() / "AngleCalibration_Test_Data" /
+                               "DummyRawFiles" / "detector_angles.bin";
+
+        REQUIRE(std::filesystem::exists(I0_data));
+        REQUIRE(std::filesystem::exists(detector_angles));
+
+        MythenFileReader file_reader(detector_angles, I0_data);
+
+        MythenFrame frame = file_reader.read_frame(mythen_data);
+
+        REQUIRE(frame.detector_angle == 1.11);
+        REQUIRE(frame.incident_intensity == 1002);
+        REQUIRE(frame.photon_counts().size() == 1280 * 2);
+        REQUIRE(frame.channel_mask == std::array<uint8_t, 3>{0, 0, 1});
+    }
+}
+
+TEST_CASE("test custom mythenfilereader", "[.mythenfilereader][.files]") {
 
     auto fpath = test_data_path() / "AngleCalibration_Test_Data";
 
     REQUIRE(std::filesystem::exists(fpath));
 
-    MythenFileReader file_reader;
+    CustomMythenFileReader file_reader;
 
     auto frame =
         file_reader.read_frame(fpath / "ang1up_22keV_LaB60p3mm_48M_a_0320.h5");
@@ -31,5 +77,7 @@ TEST_CASE("test mythenfile_reader", "[.mythenfilereader][.files]") {
 
     CHECK(frame.channel_mask == std::array<uint8_t, 3>{0, 0, 1});
 
-    CHECK(frame.photon_counts.size() == 61440);
+    CHECK(frame.photon_counts().size() == 61440);
+
+    CHECK(frame.incident_intensity == 0.0);
 }
